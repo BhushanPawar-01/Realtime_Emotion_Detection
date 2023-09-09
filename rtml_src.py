ea3c10561232f1d4db1333e00d2e73186f4c4a1b
BhushanP <102027682+BhushanPawar-01@users.noreply.github.com>
2023-09-09 21:16:50 +0530

    Added gradio to orginal src

--- rtml_src.py
+++ rtml_src.py
@@ -1,46 +1,46 @@
-from deepface import DeepFace
+import gradio as gr
 import cv2
 import numpy as np
+from deepface import DeepFace
 
-# define deepface model
+# Define deepface model
 model = DeepFace.build_model('Emotion')
 
-# define emotion labels
+# Define emotion labels
 emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
-# video capture
-cap = cv2.VideoCapture(0)
+# Function to perform emotion detection on a single frame
+def detect_emotion(frame):
+    try:
+        if frame is not None:
+            # Resize frame
+            resized_frame = cv2.resize(frame, (48, 48), interpolation=cv2.INTER_AREA)
 
-while True:
-    # capture frame-by-frame
-    ret, frame = cap.read()
+            # Convert frame to grayscale
+            gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
 
-    # resize frame
-    resized_frame = cv2.resize(frame, (48, 48), interpolation=cv2.INTER_AREA)
+            # Preprocess the image for DeepFace
+            img = gray_frame.astype('float32') / 255.0
+            img = np.expand_dims(img, axis=-1)
+            img = np.expand_dims(img, axis=0)
 
-    # convert frame to grayscale
-    gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
+            # Predict emotion using DeepFace
+            preds = model.predict(img)
+            emotion_idx = np.argmax(preds)
+            emotion = emotion_labels[emotion_idx]
 
-    # preprocess the image for DeepFace
-    img = gray_frame.astype('float32') / 255.0
-    img = np.expand_dims(img, axis=-1)
-    img = np.expand_dims(img, axis=0)
+            # Draw rectangle around face and label with predicted emotion
+            cv2.rectangle(frame, (0, 0), (200, 30), (0, 0, 0), -1)
+            cv2.putText(frame, emotion, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-    # predict emotion using DeepFace
-    preds = model.predict(img)
-    emotion_idx = np.argmax(preds)
-    emotion = emotion_labels[emotion_idx]
+        return frame
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return frame
 
-    # draw rectangle around face and label with predicted emotion
-    cv2.rectangle(frame, (0, 0), (200, 30), (0, 0, 0), -1)
-    cv2.putText(frame, emotion, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+# Create a Gradio interface
+webcam = gr.inputs.Video()
+output_video = gr.outputs.Video()
 
-    # display the result
-    cv2.imshow('Real-time Emotion Detection', frame)
-
-    # press 'q' to exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyWindow('camera')
+iface = gr.Interface(fn=detect_emotion, inputs=webcam, outputs=output_video, live=True)
+iface.launch(share=True)
